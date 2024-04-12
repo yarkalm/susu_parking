21dddf3016e18b87c80a226d62bdf26c8cb58646
yarkalm <yaroslavkalmykov01@gmail.com>
2024-04-12 16:19:17 +0500

    Create yolo files

---
+++ yolo.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+
+title = 'SUSU Parking'
+# Load the YOLOv8 model
+model = YOLO('yolov8s.pt')
+
+# Open the video file
+video_path = "https://cdn.cams.is74.ru/hls/playlists/multivariant.m3u8?uuid=52062848-61de-4e39-8948-5ca14dcfcd0b&token=bearer-aa25824cdf8db4addcce63e4b6de4e44"
+cap = cv2.VideoCapture(video_path)
+cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+# cv2.resizeWindow(title, 1600, 900)
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        h, w = frame.shape[0], frame.shape[1]
+        frame = frame[int(h*0.495):int(h*0.875), int(w*0.15):int(w*0.85)]
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
+
+
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow(title, annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            cv2.imwrite('frame.jpg', frame)
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+
+cap.release()
+cv2.destroyAllWindows()
